f4c67c7e56a178613339e941d7315cdbbad83e5c
Max Norton <maxbnorton@gmail.com>
2016-12-04 14:07:39 -0800

    generate CDNR tables

--- estimate.R
+++ estimate.R
@@ -4,7 +4,10 @@ regions <- row.names(parameters)
 scenarios <- names(yieldFactors)[2:12]
 practices <- c("np", names(parameters[1:3]))
 yieldProfiles <- array(dim=c(26,11,5), dimnames=list(yieldFactors$age,scenarios,regions))
-costProfiles <- array(dim=c(26,4,5), dimnames=list(yieldFactors$age,practices,regions))
+costProfiles <- array(dim=c(26,11,4,5), dimnames=list(yieldFactors$age,scenarios,practices,regions))
+netReturns <- array(dim=c(26,11,4,5), dimnames=list(yieldFactors$age,scenarios,practices,regions))
+discNetReturns <- array(dim=c(26,11,4,5), dimnames=list(yieldFactors$age,scenarios,practices,regions))
+cumDNR <- array(dim=c(26,11,4,5), dimnames=list(yieldFactors$age,scenarios,practices,regions))
 
 # fill out the age-yield profiles for each region, for each scenario
 for (reg in regions) {
@@ -20,13 +23,45 @@ for (reg in regions) {
 
 # fill out the age-cost profiles for each region, for each scenario
 for (reg in regions) {
-  for (prac in practices) {
-    for (year in 1:4) {
-      costProfiles[year,prac,reg] = parameters[reg,paste("cost", year-1, sep="")] - ifelse(prac=="np",0,parameters[reg,prac])
-      #not quite ready yet because the practice cost only applies if year > the scenario year
+  for (sce in scenarios) {
+    for (prac in practices) {
+      for (year in 1:26) {
+        costProfiles[year,sce,prac,reg] = parameters[reg,ifelse(year-1<4,paste("cost", year-1, sep=""),"cost3")] - ifelse(prac=="np" | sce=="healthy" | sce=="infected", 0, ifelse(year-1<as.numeric(substr(sce,5,nchar(sce))),0,parameters[reg,prac]))
+      }
     }
-    for (year in 7:26) {
-      costProfiles[year,prac,reg] = parameters[reg,"cost3"]
+  }
+}
+
+# fill out the netReturns array for each region, for each scenario, for each practice, for each year
+for (reg in regions) {
+  for (sce in scenarios) {
+    for (prac in practices) {
+      for (year in 1:26) {
+        netReturns[year,sce,prac,reg] = yieldProfiles[year,sce,reg]*parameters[reg,"price"] - costProfiles[year,sce,prac,reg]
+      }
+    }
+  }
+}
+
+# discount the netReturns tables. discount rate is hard-coded here.
+for (reg in regions) {
+  for (sce in scenarios) {
+    for (prac in practices) {
+      for (year in 1:26) {
+        discNetReturns[year,sce,prac,reg] = netReturns[year,sce,prac,reg]/(1.03^(year-1))
+      }
+    }
+  }
+}
+
+
+# sum the net returns to make the cumDNR tables: cumDNR = present returns + prev year's returns
+for (reg in regions) {
+  for (sce in scenarios) {
+    for (prac in practices) {
+      for (year in 1:26) {
+        cumDNR[year,sce,prac,reg] = discNetReturns[year,sce,prac,reg] + ifelse(year-1>0, cumDNR[year-1,sce,prac,reg],0)
+      }
     }
   }
 }
